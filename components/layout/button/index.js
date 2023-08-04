55d40e861dd7e6237af037879174967baa0f5114
Davi Augusto <davi_augusto11@hotmail.com>
2023-08-04 00:36:22 -0300

    Adjusts in navbar on links redirect for other pages

---
+++ components/layout/button/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import style from "./style.module.css";
+
+const Button = () => {
+  return (
+    <div className={style.button}>
+      <a href="">
+        <p className={style.text}>Apoie</p>
+      </a>
+    </div>
+  );
+};
+
+export default Button;
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
 
 const MyDocument = () => {
   return (
