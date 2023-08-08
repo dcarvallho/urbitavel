6012a9a30d919b6042e6878b4cdda0b410e27d51
Davi Augusto <davi_augusto11@hotmail.com>
2023-08-08 00:21:20 -0300

    Adjusts in style footer and home page

--- components/layout/footer/index.js
+++ components/layout/footer/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import style from "./style.module.css";
 
-const Footer = ()=>{
-    return(
-        <>
-            <p>Rodapé</p>
-        </>
-    )
-}
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+      <img src="/images/footer.svg" className={style.image} />
+      <div className={style.testColorFooter}></div>
+    </div>
+  );
+};
 
 export default Footer;
